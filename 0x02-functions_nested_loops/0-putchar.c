bcc9543ddddca2448d8ec472570710b035bdbaf6
DavidLc1 <davidlemah000@gmail.com>
2022-10-19 14:33:02 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,12 @@
 
 
 /**
- * main - Entry point
+ * main - print _putchar
  *
  * Return: Always 0 (Success)
  */
 
-int main(void);
+int main(void)
 {
 _putchar('_');
 _putchar('p');
@@ -16,7 +16,7 @@ _putchar('t');
 _putchar('c');
 _putchar('h');
 _putchar('a');
-i_putchar('r');
+_putchar('r');
 _putchar('\n');
 
 return (0);
