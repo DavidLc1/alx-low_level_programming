84693c88863f9837b16b8336c7a21fc4ef66f4f3
DavidLc1 <davidlemah000@gmail.com>
2022-10-18 19:53:49 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
-#include <main.h>
+#include "main.h"
+
 /**
- * main-the starting point of code
+ * main - print _putchar
  *
  * Return: 0 for Success
  */
+
 int main(void)
 {
 	_putchar("_putchar");
-
 	_putchar('\n');
 
 	return (0);
-
 }
